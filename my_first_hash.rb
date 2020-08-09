aa5eb83730a813251142be21f7579810bd9654d4
Valentina E <tiyahelfrih@gmail.com>
2020-08-09 17:06:00 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -14,6 +14,7 @@ def retrieval
                   "oil paintings" => 3
                  }
 
+  shipping_manifest["oil paintings"]
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
 end
